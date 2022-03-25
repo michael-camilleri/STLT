c89dbc9c33aa60a5bed835ab3aaee200c4a67ad7
Michael P Camilleri <michael.p.camilleri@ed.ac.uk>
2022-03-25 08:58:05 +0000

    Features
    Features:
     * Can optimise for just top 1 accuracy

--- src/utils/evaluation.py
+++ src/utils/evaluation.py
@@ -1,9 +1,10 @@
 class Evaluator:
-    def __init__(self, total):
+    def __init__(self, total, how_best='average'):
         self.corrects_top1 = 0
         self.corrects_top5 = 0
         self.total = total
         self.best_acc = 0.0
+        self.how_best = how_best.lower()
 
     def reset(self):
         self.corrects_top1 = 0
@@ -23,7 +24,14 @@ class Evaluator:
 
     def is_best(self):
         top1_accuracy, top5_accuracy = self.evaluate()
-        if (top1_accuracy + top5_accuracy) / 2 > self.best_acc:
-            self.best_acc = (top1_accuracy + top5_accuracy) / 2
+        if self.how_best == 'average':
+            accuracy = (top1_accuracy + top5_accuracy) / 2
+        elif self.how_best == 'top1':
+            accuracy = top1_accuracy
+        else:
+            accuracy = top5_accuracy
+        if accuracy > self.best_acc:
+            self.best_acc = accuracy
             return True
-        return False
+        else:
+            return False
